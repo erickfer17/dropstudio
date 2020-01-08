d7b5c76a1ee2ef5c7699e01976cbdfcf0b8e5d31
erickfer17 <erfebaro@gmail.com>
2020-01-08 17:10:52 -0600

    Upload all resources for integration carousel with navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { SlideComponent } from './slide/slide.component';
 import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    SlideComponent,
     CarouselComponent
   ],
   imports: [
